119cc9e6aad302cf9377aa736377443795962a02
Angel Lomeli <alomelibravo@gmail.com>
2024-12-18 22:52:42 -0700

    Tarea video #90 componente card-list

---
+++ src/app/gifs/card-list/card-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'gifs-card-list',
+  templateUrl: './card-list.component.html',
+})
+export class CardListComponent { }
