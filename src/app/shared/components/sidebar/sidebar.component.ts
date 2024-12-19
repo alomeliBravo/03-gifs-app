e1986eb1ec5072c3c29cb64a907e75268df73a4e
Angel Lomeli <alomelibravo@gmail.com>
2024-12-18 22:18:57 -0700

    #88 Creación del sidebar y estructura de carpetas gifs

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent { }
