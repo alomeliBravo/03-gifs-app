f09dd63c68601f04be1aba1528f6055212385951
Angel Lomeli <alomelibravo@gmail.com>
2024-12-20 02:10:02 -0700

    Documentación archivo gifs.service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -7,23 +7,31 @@ export class GifsService {
 
   constructor() { }
 
+  //Devuelve el arreglo desestructurado
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
   }
 
-  private organizeHistory(tag: string) {
-    tag = tag.toLowerCase();
 
+  //Organiza el historial de busquedas de gifs
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase(); //Convierte el input a lowercase
+
+    //Si ya existe esa busqueda regresa un nuevo arreglo donde no exista
     if ( this.tagsHistory.includes(tag)){
       this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag )
     }
 
+    //se inserta la busqueda
     this._tagsHistory.unshift( tag );
+
+    //Evita que existan más de 10 elementos en el arreglo
     this._tagsHistory = this._tagsHistory.splice(0,10);
   }
 
-  searchTag( tag: string ) {
 
+  searchTag( tag: string ) {
+    //Si el tag es un string vacío no hará anda
     if (tag.length === 0) return;
 
     this.organizeHistory(tag);
