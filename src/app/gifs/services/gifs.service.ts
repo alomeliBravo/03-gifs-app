464e06095fd4d5886c35b662ed5591d2b816bc82
Angel Lomeli <alomelibravo@gmail.com>
2024-12-20 01:57:23 -0700

    Video #93 Validaciones al servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,8 +11,23 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  searchTag( tag: string ) {
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if ( this.tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag )
+    }
+
     this._tagsHistory.unshift( tag );
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+  }
+
+  searchTag( tag: string ) {
+
+    if (tag.length === 0) return;
+
+    this.organizeHistory(tag);
+
   }
 
 }
